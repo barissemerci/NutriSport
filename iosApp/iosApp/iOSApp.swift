2389bbd9772b464daba4f5ee34ffa05dc339a158
Barış SEMERCİ <barissemerci3@gmail.com>
2025-09-14 14:34:20 +0300

    add google authentication for iOS

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,10 +1,31 @@
 import SwiftUI
+import GoogleSignIn
+import Firebase
+import FirebaseCore
+import FirebaseMessaging
+import ComposeApp
+
 
 @main
 struct iOSApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().onOpenURL(perform: {url in GIDSignIn.sharedInstance.handle(url)})
         }
     }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+           Messaging.messaging().apnsToken = deviceToken
+     }
 }
