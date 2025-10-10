290471c37f8b608267034d8f093a1d17914aee15
Barış SEMERCİ <barissemerci3@gmail.com>
2025-10-10 17:12:30 +0300

    update message bar colors

--- feature/auth/src/commonMain/kotlin/com/nutrisport/auth/AuthScreen.kt
+++ feature/auth/src/commonMain/kotlin/com/nutrisport/auth/AuthScreen.kt
@@ -24,8 +24,11 @@ import com.nutrisport.shared.Alpha
 import com.nutrisport.shared.BebasNeueFont
 import com.nutrisport.shared.FontSize
 import com.nutrisport.shared.Surface
+import com.nutrisport.shared.SurfaceBrand
+import com.nutrisport.shared.SurfaceError
 import com.nutrisport.shared.TextPrimary
 import com.nutrisport.shared.TextSecondary
+import com.nutrisport.shared.TextWhite
 import rememberMessageBarState
 
 @Composable
@@ -37,6 +40,10 @@ fun AuthScreen() {
             contentBackgroundColor = Surface,
             messageBarState = messageBarState,
             errorMaxLines = 2,
+            errorContainerColor = SurfaceError,
+            errorContentColor = TextWhite,
+            successContainerColor = SurfaceBrand,
+            successContentColor = TextPrimary,
             modifier = Modifier.padding(
                 top = paddingValues.calculateTopPadding(),
                 bottom = paddingValues.calculateBottomPadding()
